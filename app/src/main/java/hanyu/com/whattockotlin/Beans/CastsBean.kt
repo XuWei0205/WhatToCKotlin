311b8a6634fe06450a9db12624b66b2fcf36b052
HanYu <815624804@qq.com>
2018-08-23 22:54:07 +0800

    1、完善Bean

---
+++ app/src/main/java/hanyu/com/whattockotlin/Beans/CastsBean.kt
@@ -0,0 +1,13 @@
+package hanyu.com.whattockotlin.Beans
+
+/**
+ * Created by HanYu on 2018/8/23 0023.
+ */
+class CastsBean {
+    var alt: String = ""
+    var avatars: ImageBean? = null
+    var name: String = ""
+    var id: String = ""
+}
+
+
--- app/src/main/java/hanyu/com/whattockotlin/Beans/MoviesBean.kt
+++ app/src/main/java/hanyu/com/whattockotlin/Beans/MoviesBean.kt
@@ -1,80 +1,24 @@
 package hanyu.com.whattockotlin.Beans
 
+import com.google.gson.annotations.SerializedName
+
 /**
  * Created by Administrator on 2018/8/23.
  */
 class MoviesBean {
     var rating: RatingBean? = null
     var genres: ArrayList<String>? = null
-
+    var title: String? = null
+    var casts: ArrayList<CastsBean>? = null
+    @SerializedName("collect_count")
+    var collectCount: Long = 0
+    @SerializedName("original_title")
+    var originalTitle: String = ""
+    var subtype: String = ""
+    val directors: ArrayList<CastsBean>? = null
+    var year: String = ""
+    var images: ImageBean? = null
+    var art: String = ""
+    var id: String = ""
 }
 
-/*
-{
-    rating: {
-    max: 10,
-    average: 7.3,
-    stars: "40",
-    min: 0
-},
-    genres: [
-    "剧情",
-    "喜剧"
-    ],
-    title: "一出好戏",
-    casts: [
-    {
-        alt: "https://movie.douban.com/celebrity/1274242/",
-        avatars: {
-        small: "http://img3.doubanio.com/view/celebrity/s_ratio_celebrity/public/p1656.jpg",
-        large: "http://img3.doubanio.com/view/celebrity/s_ratio_celebrity/public/p1656.jpg",
-        medium: "http://img3.doubanio.com/view/celebrity/s_ratio_celebrity/public/p1656.jpg"
-    },
-        name: "黄渤",
-        id: "1274242"
-    },
-    {
-        alt: "https://movie.douban.com/celebrity/1138320/",
-        avatars: {
-        small: "http://img3.doubanio.com/view/celebrity/s_ratio_celebrity/public/p727.jpg",
-        large: "http://img3.doubanio.com/view/celebrity/s_ratio_celebrity/public/p727.jpg",
-        medium: "http://img3.doubanio.com/view/celebrity/s_ratio_celebrity/public/p727.jpg"
-    },
-        name: "舒淇",
-        id: "1138320"
-    },
-    {
-        alt: "https://movie.douban.com/celebrity/1274388/",
-        avatars: {
-        small: "http://img7.doubanio.com/view/celebrity/s_ratio_celebrity/public/p1356403251.95.jpg",
-        large: "http://img7.doubanio.com/view/celebrity/s_ratio_celebrity/public/p1356403251.95.jpg",
-        medium: "http://img7.doubanio.com/view/celebrity/s_ratio_celebrity/public/p1356403251.95.jpg"
-    },
-        name: "王宝强",
-        id: "1274388"
-    }
-    ],
-    collect_count: 472611,
-    original_title: "一出好戏",
-    subtype: "movie",
-    directors: [
-    {
-        alt: "https://movie.douban.com/celebrity/1274242/",
-        avatars: {
-        small: "http://img3.doubanio.com/view/celebrity/s_ratio_celebrity/public/p1656.jpg",
-        large: "http://img3.doubanio.com/view/celebrity/s_ratio_celebrity/public/p1656.jpg",
-        medium: "http://img3.doubanio.com/view/celebrity/s_ratio_celebrity/public/p1656.jpg"
-    },
-        name: "黄渤",
-        id: "1274242"
-    }
-    ],
-    year: "2018",
-    images: {
-    small: "http://img7.doubanio.com/view/photo/s_ratio_poster/public/p2529571873.jpg",
-    large: "http://img7.doubanio.com/view/photo/s_ratio_poster/public/p2529571873.jpg",
-    medium: "http://img7.doubanio.com/view/photo/s_ratio_poster/public/p2529571873.jpg"
-},
-    alt: "https://movie.douban.com/subject/26985127/",
-    id: "26985127"
-},*/
