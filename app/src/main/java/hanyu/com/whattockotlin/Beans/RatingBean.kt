d0db616c57405f5acbe57c0ff790b7425192d1db
HanYu <815624804@qq.com>
2018-08-23 18:03:27 +0800

    1、增加Bean文件

---
+++ app/src/main/java/hanyu/com/whattockotlin/Beans/RatingBean.kt
@@ -0,0 +1,11 @@
+package hanyu.com.whattockotlin.Beans
+
+/**
+ * Created by Administrator on 2018/8/23.
+ */
+class RatingBean {
+    var max: Float = 0.0f
+    var average: Float = 0.0f
+    var stars: Float = 0.0f
+    var min: Float = 0.0f
+}
---
+++ app/src/main/java/hanyu/com/whattockotlin/Beans/SubjectBean.kt
@@ -0,0 +1,12 @@
+package hanyu.com.whattockotlin.Beans
+
+/**
+ * Created by Administrator on 2018/8/23.
+ */
+class SubjectBean {
+    var subjects: ArrayList<MoviesBean>? = null
+    var count: Int = 0
+    var start: Int = 0
+    var total: Int = 0
+    var title: String? = null
+}
