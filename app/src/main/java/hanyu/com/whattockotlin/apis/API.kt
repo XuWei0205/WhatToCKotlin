f342af1f13e3ba7534e8cd89790b057aa4093b8d
HanYu <18274983767@163.com>
2018-08-23 23:54:29 +0800

    1、增加基础接口

--- app/src/main/java/hanyu/com/whattockotlin/apis/API.kt
+++ app/src/main/java/hanyu/com/whattockotlin/apis/API.kt
@@ -4,4 +4,7 @@ package hanyu.com.whattockotlin.apis
  * Created by Administrator on 2018/8/23.
  */
 class API {
+    companion object {
+        val BASE_URL: String = "http://api.douban.com"
+    }
 }
--- app/src/main/java/hanyu/com/whattockotlin/apis/IAPI.kt
+++ app/src/main/java/hanyu/com/whattockotlin/apis/IAPI.kt
@@ -1,8 +1,21 @@
 package hanyu.com.whattockotlin.apis
 
+import hanyu.com.whattockotlin.beans.SubjectBean
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
 /**
  * Created by Administrator on 2018/8/23.
  */
 interface IAPI {
+    /**获取最近电影列表
+     * @param start 开始条目
+     * @param count 数量
+     * @param city 所在地区
+     * **/
+
+    @GET("/v2/movie/in_theaters")
+    fun getLatestMovie(@Query("start") start: Int, @Query("count") count: Int, @Query("city") city: String): Call<SubjectBean>
 
 }
