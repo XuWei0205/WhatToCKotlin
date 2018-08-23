c1f0c375062dfb71c684279479bafcc5549ff951
HanYu <815624804@qq.com>
2018-08-23 23:00:41 +0800

    1、补交ImageBean

---
+++ app/src/main/java/hanyu/com/whattockotlin/Beans/ImageBean.kt
@@ -0,0 +1,10 @@
+package hanyu.com.whattockotlin.Beans
+
+/**
+ * Created by HanYu on 2018/8/23 0023.
+ */
+class ImageBean {
+    var small: String = ""
+    var large: String = ""
+    var medium: String = ""
+}
