46d3c10ff6b94a1b1bceaa1034dbe83003c208b1
HanYu <18274983767@163.com>
2018-08-24 15:58:34 +0800

    1、完善加载圆角图片

---
+++ app/src/main/java/hanyu/com/whattockotlin/commons/GlideRoundTransformation.kt
@@ -0,0 +1,47 @@
+package hanyu.com.whattockotlin.commons
+
+import android.content.Context
+import android.graphics.*
+import com.bumptech.glide.load.engine.bitmap_recycle.BitmapPool
+import com.bumptech.glide.load.resource.bitmap.BitmapTransformation
+
+/**
+ * Created by HanYu on 2018/8/24.
+ */
+class GlideRoundTransformation : BitmapTransformation {
+    private var radius: Float = 0.0f
+
+
+    constructor(context: Context, radius: Float) : super(context) {
+        this@GlideRoundTransformation.radius = radius
+    }
+
+    constructor(context: Context) : super(context) {
+        this@GlideRoundTransformation.radius = 5f
+    }
+
+
+    override fun getId(): String {
+        return javaClass.name
+    }
+
+    override fun transform(pool: BitmapPool?, toTransform: Bitmap?, outWidth: Int, outHeight: Int): Bitmap {
+        return doTransform(pool!!, toTransform!!)
+    }
+
+    private fun doTransform(pool: BitmapPool, toTransform: Bitmap): Bitmap {
+
+        var result: Bitmap? = pool.get(toTransform.width, toTransform.height, Bitmap.Config.ARGB_8888)
+        if (result == null) {
+            result = Bitmap.createBitmap(toTransform.width, toTransform.height, Bitmap.Config.ARGB_8888)
+        }
+
+        val canvas = Canvas(result!!)
+        val paint = Paint()
+        paint.shader = BitmapShader(toTransform, Shader.TileMode.CLAMP, Shader.TileMode.CLAMP)
+        paint.isAntiAlias = true
+        val rectF = RectF(0f, 0f, toTransform.width.toFloat(), toTransform.height.toFloat())
+        canvas.drawRoundRect(rectF, radius, radius, paint)
+        return result
+    }
+}
